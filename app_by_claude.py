2419e0acee04222d44a119757ef817da6c207dae
Gilito21 <164108913+Gilito21@users.noreply.github.com>
2025-03-09 04:08:29 +0100

    Update app_by_claude.py

--- app_by_claude.py
+++ app_by_claude.py
@@ -65,7 +65,7 @@ def extract_frames(video_path, output_folder, frame_interval=5):
         'ffmpeg',
         '-i', video_path,
         '-vf', filter_expr,
-        '-vsync', '0',  # Variable frame rate
+        '-vsync', 'vfr',  # Variable frame rate
         '-q:v', '1',  # Quality level (lower is better)
         f'{output_folder}/frame_%04d.jpg'
     ]
